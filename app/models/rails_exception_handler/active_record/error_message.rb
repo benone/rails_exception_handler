147710d3dcf6331ba99d5e2cb1e6a3a88e235cff
Кирилл <hi.sender@gmail.com>
2016-12-27 13:54:17 +0300

    Full connection params

--- app/models/rails_exception_handler/active_record/error_message.rb
+++ app/models/rails_exception_handler/active_record/error_message.rb
@@ -2,7 +2,9 @@ class RailsExceptionHandler::ActiveRecord
 
   class ErrorMessage < defined?(ActiveRecord) ? ActiveRecord::Base : Object
     if (defined?(ActiveRecord) && RailsExceptionHandler.configuration.activate? && RailsExceptionHandler.configuration.active_record?)
-      establish_connection(RailsExceptionHandler.configuration.active_record_store_in[:database])
+      db_conf = YAML.load_file(Rails.root.join("config/database.yml"))
+      spec = db_conf['exception_database'] 
+      establish_connection(spec)
       self.table_name = RailsExceptionHandler.configuration.active_record_store_in[:record_table]
     end
   end
